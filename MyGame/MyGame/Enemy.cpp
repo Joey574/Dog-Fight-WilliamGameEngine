67a79d83d2b59aa08156379c5cc4c0da8b357b16
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2022-06-02 08:54:39 -0700

    mhgdn

--- MyGame/MyGame/Enemy.cpp
+++ MyGame/MyGame/Enemy.cpp
@@ -14,10 +14,11 @@ const int WINDOW_HEIGHT = 600;
 
 Enemy::Enemy()
 {
-	sprite_.setRotation(180);
+	sprite_.setRotation(135);
 	sprite_.setOrigin(sf::Vector2f(56.5, 56.5));
 	sprite_.setTexture(GAME.getTexture("Resources/enemy.png"));
-	sprite_.setPosition(sf::Vector2f(730, 530));
+	//sprite_.setPosition(sf::Vector2f(730, 530));
+	sprite_.setPosition(sf::Vector2f(500, 265));
 	assignTag("enemy");
 }
 
