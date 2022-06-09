5a5c4e4570f9cf0fb59d24044f0940088730a483
Ericpace644 <90404067+Ericpace644@users.noreply.github.com>
2022-06-09 07:25:46 -0700

    hft

--- MyGame/MyGame/AI.h
+++ MyGame/MyGame/AI.h
@@ -1,28 +1,28 @@
-#pragma once
-
-#include "Engine//GameEngine.h"
-
-class AI : public GameObject
-{
-public:
-
-	AI();
-
-	void draw();
-	void update(sf::Time& elapsed);
-
-	sf::FloatRect getCollisionRect();
-	void handleCollision(GameObject& otherGameObject);
-
-	void edgeCheck(float x, float y);
-	void rotationCheck(float& tempW, float& tempH, int rotation);
-	void rotationSet(bool up, bool down, bool left, bool right);
-
-private:
-	sf::Sprite sprite_;
-	int fireTimer_ = 0;
-	int weapon_ = 1;
-	int weaptime_ = 10;
-};
-
-typedef std::shared_ptr<AI> AIPtr;
+//#pragma once
+//
+//#include "Engine//GameEngine.h"
+//
+//class AI : public GameObject
+//{
+//public:
+//
+//	AI();
+//
+//	void draw();
+//	void update(sf::Time& elapsed);
+//
+//	sf::FloatRect getCollisionRect();
+//	void handleCollision(GameObject& otherGameObject);
+//
+//	void edgeCheck(float x, float y);
+//	void rotationCheck(float& tempW, float& tempH, int rotation);
+//	void rotationSet(bool up, bool down, bool left, bool right);
+//
+//private:
+//	sf::Sprite sprite_;
+//	int fireTimer_ = 0;
+//	int weapon_ = 1;
+//	int weaptime_ = 10;
+//};
+//
+//typedef std::shared_ptr<AI> AIPtr;
